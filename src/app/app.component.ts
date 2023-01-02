3a9d22c6aa0388d6ac404b5f6f39b729ecd3d544
Radu Maftei <mafteiradu23@yahoo.com>
2023-01-02 18:53:38 +0200

    AI generated images done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, isDevMode} from '@angular/core';
+
+import { Configuration, OpenAIApi } from "openai";
+import {environment} from "../environments/environment";
+
+const hasOwnPropFailSafe = (obj: any, prop: string) => {
+  return obj.hasOwnProperty(prop) ? obj[prop] : '';
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  openai!: OpenAIApi;
   title = 'dall-e';
+  imageSearch = '';
+  numberOfImages?: number;
+  openAIImageResults: string[] = [];
+  apiKey?: string = hasOwnPropFailSafe(environment, 'openAIKey')
+  fetchingImages = false;
+
+  constructor() {
+    if (this.apiKey) {
+      const configuration = new Configuration({
+        apiKey: this.apiKey
+      });
+
+      this.openai = new OpenAIApi(configuration);
+    }
+  }
+
+  get disabledButtonState(): boolean {
+    const parsedNumberOfImages = parseInt(this.numberOfImages?.toString() || '');
+
+    return !this.imageSearch.length ||
+      (typeof this.numberOfImages !== "number"
+        || !(parsedNumberOfImages > 0 && parsedNumberOfImages < 6));
+   }
+
+  async generateImage() {
+    this.fetchingImages = true;
+
+    const result = await this.openai.createImage({
+      prompt: this.imageSearch,
+      n: this.numberOfImages,
+      size: '512x512'
+    });
+
+    this.openAIImageResults = result.data.data.map(e => e.url) as string[];
+
+    this.fetchingImages = false;
+  }
+
+  addEmoji($event: any) {
+    console.log('EMOJIJ ', $event.emoji.native)
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { PickerModule } from '@ctrl/ngx-emoji-mart';
 
 import { AppComponent } from './app.component';
+
+// PrimeNG stuff
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {InputNumberModule} from "primeng/inputnumber";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    PickerModule,
+    InputNumberModule
   ],
   providers: [],
   bootstrap: [AppComponent]
